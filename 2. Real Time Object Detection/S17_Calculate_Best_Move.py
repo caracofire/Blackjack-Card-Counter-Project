b69b505267ee6b4230be7092533922793bc040d9
caracofire <leonardomiguelsp@gmail.com>
2024-07-20 23:49:18 -0400

    Initial commit

---
+++ 2. Real Time Object Detection/S17_Calculate_Best_Move.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+dealer_detections = [['QS'],[5]]
+player_detections = [['AS','AC'],[5,9]]
+dealer_hand_total = 10
+player_hand_total = 12
+count= 3
+
+
+# In[2]:
+
+
+options = ['Hit','Stand','Double','Split']
+
+
+# In[3]:
+
+
+#calculate sum of all non-ace cards in hand first
+
+def calculate_hand(hand_total,x):
+    if x.isalpha(): #J,Q,K 
+        hand_total+=10
+    else: #2,3,4,5,6,7,8,9,10
+        hand_total=hand_total+int(x)
+    return hand_total
+
+
+# In[20]:
+
+
+#this function handles calculating the soft/hard totals of a hand given a list of detections
+
+#Calculate the hand's soft/hard total if it contains aces
+
+def hand_total(input_hand):
+    detections_list=input_hand
+    ace_count = 0 
+    card_total = 0
+    soft_total = False
+    for i in detections_list:
+        i = str(i)[:-1] #trim the suit character from each string in the list
+        
+        if (i =='A'): #count number of aces in hand
+            ace_count+=1
+        else: #only add the non-aces to the card total first
+            card_total = calculate_hand(card_total,i)
+
+    #adding aces to card total IF they exist:
+    remainder = 21-card_total
+    ace_11 = 0
+    
+    # >1 ace in hand
+    if (ace_count>0 and remainder/11>=1):
+        ace_11 = 1
+        ace_count -= 1 #subtract the ace that became '11'
+ 
+        #if having an ace == 11 causes a bust, then every ace must equal 1
+    
+        if ((card_total + ace_11*11 + (ace_count))>21): 
+            #convert the ace_11 to == 1 
+            ace_11 -=1
+            ace_count +=1
+        
+        card_total = card_total + ace_count * 1 + ace_11 * 11
+    else: 
+        card_total = card_total + ace_count * 1
+    
+    if ace_11>0:
+        soft_total=True
+    
+    return card_total, soft_total
+
+
+# In[5]:
+
+
+def is_hand_pair(player_detections):
+
+    if (str(player_detections[0])[:-1]==str(player_detections[1])[:-1]):
+        
+        return True
+
+    
+
+
+# In[6]:
+
+
+def is_hand_soft_total(player_detections):
+    if hand_total(player_detections)[1]: #take the first return of hand_total which returns soft_total or not
+        return 'Soft'
+    else:
+        return 'Hard'
+                                            
+
+
+# In[7]:
+
+
+def soft_totals(player_detections,player_hand_total, dealer_total, count):
+    
+    
+    if player_hand_total == 20:
+        return 'Stand'
+    elif player_hand_total == 19:
+        if dealer_total == 4:
+            if count >= 3:
+                return 'Double'
+            else:
+                return 'Stand'
+        elif dealer_total == 5:
+            if count >=1:    
+                return 'Double'
+            else:
+                return 'Stand'
+        elif dealer_total == 6:
+            if count >=1:
+                return 'Double'
+            else:
+                return 'Stand'
+        else:
+            return 'Stand'
+    elif player_hand_total == 18:
+        if dealer_total>=2 and dealer_total<=6:
+            return 'Double'
+        elif dealer_total in {7,8}:
+            return 'Stand'
+        else: 
+            return 'Hit'
+    
+    elif player_hand_total == 17:
+        if (dealer_total == 2):
+            if count >=1:
+                return 'Double'
+            else: 
+                return 'Hit'
+                
+        elif (dealer_total>=3 and dealer_total<=6):
+            return 'Double'
+        else:
+            return 'Hit'
+        
+    elif player_hand_total in {16,15}:
+        if dealer_total>=4 and dealer_total<=6:
+            return 'Double'
+        else: 
+            return 'Hit'
+    elif player_hand_total in {14,13}:
+        if dealer_total in [5,6]:
+            return 'Double'
+        else: 
+            return 'Hit'
+
+
+# In[8]:
+
+
+def hard_totals(player_detections,player_hand_total, dealer_total, count):
+    if player_hand_total >=17 and player_hand_total <= 21:
+        return 'Stand'
+    elif player_hand_total == 16:
+        if dealer_total == 9:
+            if count >=4:
+                return 'Stand'
+            else:
+                return 'Hit'
+        elif dealer_total == 10:
+            if count >0:
+                return 'Stand'
+            else: 
+                return 'Hit'
+        elif dealer_total in {7,8,11}:
+            return 'Hit'
+        else:
+            return 'Stand'
+    elif player_hand_total == 15:
+        if dealer_total == 10:
+            if count >=4:
+                return 'Stand'
+            else: 
+                return 'Hit'
+            
+        elif dealer_total>=2 and dealer_total<=6:
+            return 'Stand'
+        else: 
+            return 'Hit'
+    elif player_hand_total == 14:
+        if dealer_total>=2 and dealer_total <=6:
+            return 'Stand'
+        else: 
+            return 'Hit'
+
+    elif player_hand_total == 13:
+        if dealer_total ==2:
+            if count<=-1:
+                return 'Hit'
+            else: 
+                return 'Stand'
+        
+        elif dealer_total>=3 and dealer_total <=6:
+            return 'Stand'
+        else: 
+            return 'Hit'
+    
+    elif player_hand_total == 12:
+        if dealer_total ==2:
+            if count >=3:
+                return 'Stand'
+            else:
+                return 'Hit'
+        elif dealer_total == 3:
+            if count >=2:
+                return 'Stand'
+            else:
+                return 'Hit'
+        elif dealer_total == 4:
+            if count <=0:
+                return 'Hit'
+            else:
+                return 'Stand'
+        elif dealer_total in {5,6}:
+            return 'Stand'
+        elif dealer_total>=6 and dealer_total<=11:
+            return 'Hit'
+        else: 
+            return 'Hit'
+        
+    elif player_hand_total == 11:
+        if dealer_total == 11:
+            if count >=1:
+                return 'Double'
+            else:
+                return 'Hit'
+        else:
+            return 'Double'
+    
+    elif player_hand_total == 10:
+        if (dealer_total in {10,11}):
+            if count >=4:
+                return 'Double'
+            else: 
+                return 'Hit'
+        else:
+            return 'Double'
+    elif player_hand_total == 9:
+        if dealer_total == 7:
+            if count >= 3:
+                return 'Double'
+            else:
+                return 'Hit'
+        elif dealer_total == 2:
+            if count >=1:
+                return 'Double'
+            else:
+                return 'Hit'
+        elif dealer_total >=3 and dealer_total <=6:
+            return 'Double'
+        else: 
+            return 'Hit'
+            
+    elif player_hand_total == 8:
+        if dealer_total == 6 and count >=2:
+            return 'Double'
+        else:
+            return 'Hit'
+    else:
+        return 'Hit'
+    
+
+
+# In[18]:
+
+
+def soft_17(player_detections, dealer_hand_total, count):
+    
+    player_detections=player_detections[0]
+    player_hand_size = len(player_detections)
+   
+
+    player_hand_total, soft_total = hand_total(player_detections)
+    
+    
+    decision= None
+    
+    #Step 1: Check if hand is == 2 cards
+    
+    if player_hand_size == 2:
+        #Step 2: Check if hand is a pair
+        if is_hand_pair(player_detections):
+            #if it is a pair, then call pairs()
+            decision = pairs(player_detections, dealer_hand_total, count)
+            
+        else:
+            decision = 'Continue'
+            
+            
+    if player_hand_size >2 or decision == 'Continue':
+        #step 3 check if hand total is soft
+
+        
+        if soft_total == True:
+            decision = soft_totals(player_detections,player_hand_total, dealer_hand_total, count)
+            if (decision == 'Double') and (player_hand_size >2):
+                decision = 'Hit'
+        else: #if it is a hard total, calculate hard total
+            decision = hard_totals(player_detections,player_hand_total, dealer_hand_total, count)    
+            if (decision == 'Double') and (player_hand_size >2):
+                decision = 'Hit'
+            
+        
+    return decision    
+    
+    
+
+
+# In[12]:
+
+
+def pairs(player_detection, dealer_total, count):
+    player_pair = player_detection[0][:-1]
+
+    
+   
+    if player_pair in ('A','8'):
+        return 'Split'
+    if player_pair == 'A':
+        return 'Split'
+    
+    elif player_pair == '10':
+        if count <4:
+            return 'Stand'
+        else:
+            if dealer_total == 6 and count >=4:
+                return 'Split'
+            elif dealer_total == 5 and count >=5:
+                return 'Split'
+            elif dealer_total == 4 and count >=6:
+                return 'Split'
+            else:
+                return 'Stand'
+            
+    elif player_pair == '9':
+        if dealer_total in [7,10,11]:
+            return 'Stand'
+        else:
+            return 'Split'
+        
+    elif player_pair =='7':
+        if dealer_total in [8,9,10,11]:
+            return 'Continue'
+        else:
+            return 'Split' 
+    
+    elif player_pair == '6':
+        if dealer_total in (3,4,5,6):
+            return 'Split'
+        else:
+            return 'Continue' 
+        
+    elif player_pair in ('5','4'): #Double after stand is typically not seen in my experience
+        return 'Continue'
+    
+    elif player_pair in ('3','2'):
+        if (dealer_total in (4,5,6,7)):
+            return 'Split'
+        else:
+            return 'Continue'
+
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ README.md
@@ -0,0 +1,53 @@
+# Blackjack-Card-Counter-Object-Detection
+
+
+
+This program is designed to count cards and calculate the best possible move for the player in a live Blackjack game. A neural network was trained using the Tensorflow 2.9.1 Object Detection API to detect playing cards and the detection is performed on an OpenCV 4.6.0 webcam feed.
+
+
+
+# Mode 1: Card Counting Practice
+* The purpose of this mode is to allow user's to practice card counting by displaying the current deck's count.
+
+![Alt Text](https://media.giphy.com/media/yDYbF1KRknihaDIqVb/giphy-downsized-large.gif)
+
+# Mode 2: Dealer vs. Player
+* This mode calculates the player's best next move based on the overall deck's count, player and dealer's hand totals.
+* NOTE: This is based on the Soft 17 Blackjack Deviation.
+
+![Alt Text](https://github.com/NpEric360/Blackjack-Card-Counter-Object-Detection/blob/main/pvd.gif)
+
+# There are two main sections for this project:
+
+## 1. Install TensorFlow Object Detection API to Train a Custom Neural Network
+* Train a neural network to detect all 52 types of playing cards in a deck. 
+* The dataset that I used for this model is from: https://universe.roboflow.com/augmented-startups/playing-cards-ow27d/dataset/1
+
+<img src="https://i.imgur.com/sFVkPm7.png"/>
+
+
+* The neural network architecture that I chose to train for this application is the SSD_RESNET152_V1_FPN_640x640_COCO17_TPU8
+* My script will go through the entire process of creating the label map, TFRecords, and model pipeline config files required to train any Tensorflow 2 model for this specific application.
+* You will observe the model's training process using Tensorboard: 
+
+<img src = "https://i.imgur.com/ABy3j4Z.png"/> 
+
+
+* Once you have successfully trained the model without finding any concerns in the training process, you will export the frozen graph to use it in part 2.
+* I also included a section on how to export this as a TFLite model if you intend on deploying to a mobile device. NOTE: Not all model architectures are compatible in the TFLite format due to conflicting ops.
+
+## 2. Perform Real time object detection:
+* You will load the trained neural network model from part one and feed the inputs from a webcam feed to perform object detection. 
+* OpenCV is used to handle the webcam input, draw the bounding boxes around detected cards, and the corresponding game information required by the user.
+* When you feed the neural network a frame containing any one of the 52 playing cards, the neural network will output the following:
+  1. Which class/card it believes that the detected object is
+  2. The confidence/detection score of this detection
+  3. The coordinates of the box that is bounding the detection
+* With these output parameters, you can map out what the card is, and set a threshold on the detection score in order to actually display a bounding box.
+
+
+# Current Limitations:
+1. The count is based on a single deck game. You can easily scale the count by dividing by the total number of decks in the shoe.
+2. Multiple deck games will also need to the program to allow detections of multiple identical cards.
+3. As for any object detection model, speed and accuracy of the object detection model will always be a limiting factor in performance. I intend on training the YOLOV5 model for this application in the near future.
+4. Need to allow seperate hand totals and next moves for a player who splits his hand into two.
